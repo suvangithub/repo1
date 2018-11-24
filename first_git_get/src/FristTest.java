77a556ee9b010cdf26fdf0a186290ea0a5b68b3a
git <983137336@qq.com>
2018-11-24 22:52:20 +0800

    第一次提交fristTest.java文件

---
+++ first_git_get/src/FristTest.java
@@ -0,0 +1,14 @@
+/**
+ * @author William.shi
+ * @date 2018/11/24 22:47
+ */
+public class FristTest {
+
+    public static void main(String[] args) {
+        System.out.println("111111111111111");
+        System.out.println("111111111111111");
+        System.out.println("111111111111111");
+        System.out.println("111111111111111");
+        System.out.println("111111111111111");
+    }
+}
